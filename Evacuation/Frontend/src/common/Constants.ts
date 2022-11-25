cd61d3c8f6a173faa24915fdea670e7d2ff13a58
Loc Nguyen <vlocnguyen@live.dk>
2022-11-25 15:54:10 +0100

    tesst

--- Evacuation/Frontend/src/common/Constants.ts
+++ Evacuation/Frontend/src/common/Constants.ts
@@ -2,7 +2,7 @@ const constansts = {
     //serverBaseURL: 'http://localhost:3000'
     //serverBaseURL: 'https://evac-backend-uvlznjf2tq-lz.a.run.app'
     //serverBaseURL: 'https://evac-backend2-uvlznjf2tq-lz.a.run.app'
-    serverBaseURL: 'http://34.160.193.201/' // backend
+    serverBaseURL: 'https://34.149.43.63/' // backend
 
     // backend-lb ip here?
     
